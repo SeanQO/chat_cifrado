02d31d0350463077d59557669a1f943c9cdc7520
Sebastian <1144105003@u.icesi.edu.co>
2022-12-01 01:16:42 -0500

    more understandable

--- src/client/Cliente.java
+++ src/client/Cliente.java
@@ -4,6 +4,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,21 +27,27 @@ public class Cliente {
             
             Socket sc = new Socket("127.0.0.1", 5000);
             
-            int mod = 7;
-
-			int base = 5;
+            
 
             
             DataInputStream in = new DataInputStream(sc.getInputStream());
             DataOutputStream out = new DataOutputStream(sc.getOutputStream());
             
-            System.out.println("Por favor ingresa tu clave");
-            String clave = sn.nextLine();
-            int claveC = Integer.parseInt(clave);
+//            System.out.println("Por favor ingresa tu clave");
+//            String clave = sn.nextLine();
+//            int claveC = Integer.parseInt(clave);
+            int[] primos = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
+            int pos = (int) Math.floor(Math.random()*primos.length-1);
+            int mod = Integer.parseInt(in.readUTF());
+            int base = Integer.parseInt(in.readUTF());
+            int claveC = primos[pos];
+            System.out.println("Tu clave privada ha sido generada: " + claveC);
             
-    
-            
+            Thread.sleep(500);
+            System.out.println("Se esta Generando tu clave publica");
             double r = (Math.pow(base, claveC))%mod;
+            Thread.sleep(500);
+            System.out.println("Tu clave publica es: " + r + ", la cual sera compartida hasta el otro usuario");
             String publica = String.valueOf(r);
             out.writeUTF(publica);
             String publicaP = in.readUTF();
@@ -49,15 +56,36 @@ public class Cliente {
             int solucion = (int) ((Math.pow(publicPartner, claveC))%mod);
             
             String solucionStr = String.valueOf(solucion);
-            
+            Thread.sleep(500);
+            System.out.println("Ahora, mediante el algoritmo de Deffie-Hellman, se esta comprobando si la otra persona es quien tu crees");
             out.writeUTF(solucionStr);
             String solucionPartner = in.readUTF();
+			Thread.sleep(125);
+			System.out.print(".");
+			Thread.sleep(125);
+			System.out.print(".");
+			Thread.sleep(125);
+			System.out.print(".");
+			Thread.sleep(125);
+			System.out.print(".");
+			Thread.sleep(125);
+			System.out.print(".");
+			Thread.sleep(125);
+			System.out.print(".");
+			Thread.sleep(125);
+			System.out.print(".");
+			Thread.sleep(125);
+			System.out.print(".");
+			System.out.println();
             if (!solucionStr.equals(solucionPartner)) {
+            	System.out.println("oh no, el otro usuario no era quien tu creias! por tu seguridad se cerrara la sesión");
             	sc.close();
             }
             
-            
-            while(true) {
+            System.out.println("Perfecto!! se comprobo que cada quien es quien dice ser gracias a las claves simetricas, pueden continuar con su conversación!!");
+            System.out.println("Recuerda que todo mensaje enviado y recibido se cifrara mediante aes de 128bits");
+			System.out.println("Ademas, se te mostrara el mensaje cifrado justo arriba del mensaje decifrado localmente");
+			while(true) {
         	
         		String msg = in.readUTF();
         		String desencriptado = encriptador.desencriptar(msg, claveEncriptacion);
--- src/server/Servidor.java
+++ src/server/Servidor.java
@@ -19,7 +19,7 @@ import aes.AES;
 
 public class Servidor {
 
-	public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+	public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InterruptedException {
 
 		try {
 			ServerSocket server = new ServerSocket(5000);
@@ -39,15 +39,28 @@ public class Servidor {
 
 				DataInputStream in = new DataInputStream(sc.getInputStream());
 				DataOutputStream out = new DataOutputStream(sc.getOutputStream());
-				int mod = 7;
-				int base = 5;
 				
-				System.out.println("Por favor ingresa tu clave");
-				String claveS = sn.nextLine();
-				int clave = Integer.parseInt(claveS);
+				
+//				System.out.println("Por favor ingresa tu clave");
+//				String claveS = sn.nextLine();
+//				int clave = Integer.parseInt(claveS);
 
-				
+
+				int[] primos = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
+	            int pos = (int) Math.floor(Math.random()*primos.length-1);
+	            int mod = (int) Math.floor(Math.random()*primos.length-1);
+				int base = (int) Math.floor(Math.random()*primos.length-1);
+				out.writeUTF(mod + "");
+				out.writeUTF(base + "");
+	            int clave = primos[pos];
+	            System.out.println("Tu clave privada ha sido generada: " + clave);
+	            
+	            Thread.sleep(500);
+	            System.out.println("Se esta Generando tu clave publica");
+	            
 				double r = (Math.pow(base, clave))%mod;
+				Thread.sleep(500);
+	            System.out.println("Tu clave publica es: " + r + ", la cual sera compartida hasta el otro usuario");
 				String publica = String.valueOf(r);
 				out.writeUTF(publica);
 				String publicaP = in.readUTF();
@@ -56,15 +69,39 @@ public class Servidor {
 				int solucion = (int) ((Math.pow(publicPartner, clave))%mod);
 				
 				String solucionStr = String.valueOf(solucion);
-				
+				Thread.sleep(500);
+	            System.out.println("Ahora, mediante el algoritmo de Deffie-Hellman, se esta comprobando si la otra persona es quien tu crees");
+
 				out.writeUTF(solucionStr);
 				String solucionPartner = in.readUTF();
-				
+				Thread.sleep(125);
+				System.out.print(".");
+				Thread.sleep(125);
+				System.out.print(".");
+				Thread.sleep(125);
+				System.out.print(".");
+				Thread.sleep(125);
+				System.out.print(".");
+				Thread.sleep(125);
+				System.out.print(".");
+				Thread.sleep(125);
+				System.out.print(".");
+				Thread.sleep(125);
+				System.out.print(".");
+				Thread.sleep(125);
+				System.out.print(".");
+				System.out.println();
 				 if (!solucionStr.equals(solucionPartner)) {
-		            	sc.close();
+		            	System.out.println("oh no, el otro usuario no era quien tu creias! por tu seguridad se cerrara la sesión");
+					 	sc.close();
+		            	
 		            }
 				
-				// Pido al cliente el nombre al cliente
+				 
+				System.out.println("Perfecto!! se comprobo que cada quien es quien dice ser gracias a las claves simetricas, pueden continuar con su conversación!!");
+				
+				System.out.println("Recuerda que todo mensaje enviado y recibido se cifrara mediante aes de 128bits");
+				System.out.println("Ademas, se te mostrara el mensaje cifrado justo arriba del mensaje decifrado localmente");
 				while(true) {
 					String msg = sn.next();
 					String encriptado = encriptador.encriptar(msg, claveEncriptacion);
